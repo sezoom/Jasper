c821f68707a985232c21e4b78bb639e85079680f
Hamad ALSHEHHI <sezoom@gmail.com>
2022-10-04 09:01:46 +0400

    fixing bug

--- jasper.py
+++ jasper.py
@@ -866,7 +866,7 @@ def mainmenu():
                                                              packetStructure(pkt=pkt)
                                                         else:
                                                             if (inp == "ad"):
-                                                                packetConversations(pkt=pkt)
+                                                                packetConversations()
                                                             else:
                                                                 if (inp == "pd"):
                                                                     convertToDataframe(pkt=pkt)
